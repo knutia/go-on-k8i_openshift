514c8247af0e54a4c26fd687577a8c85bf842188
Knut Ivar Alvestad <knut.ivar.alvestad@cgi.com>
2019-02-25 11:26:30 +0100

    change func name

--- handlers/root.go
+++ handlers/root.go
@@ -6,6 +6,6 @@ import (
 )
 
 // home is a simple HTTP handler function which writes a response.
-func home(w http.ResponseWriter, _ *http.Request) {
+func root(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprint(w, "Hello! Your request was processed.")
 }
