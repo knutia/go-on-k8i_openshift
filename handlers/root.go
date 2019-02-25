cae66ce1f37fdb17ee212522cae9796577bd86d6
Knut Ivar Alvestad <knut.ivar.alvestad@cgi.com>
2019-02-25 11:33:58 +0100

    aDD HOSTNAME text to root page

--- handlers/root.go
+++ handlers/root.go
@@ -3,9 +3,11 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 // home is a simple HTTP handler function which writes a response.
 func root(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprint(w, "Hello! Your request was processed.")
+	host := os.Getenv("HOSTNAME")
+	fmt.Fprint(w, "Hello! Your request was processed by "+host+".")
 }
