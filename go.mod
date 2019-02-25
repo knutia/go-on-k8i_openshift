9a8f4156b0556077bc51e70ce77d7856dcf2f736
Knut Ivar Alvestad <knut.ivar.alvestad@cgi.com>
2019-02-25 10:46:02 +0100

    init

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/knutia/go-on-k8i_openshift
+
+require (
+	github.com/gorilla/mux v1.7.0
+	github.com/rumyantseva/advent-2017 v0.0.0-20180207144541-88a8e8066971
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/knutia/go-on-k8i_openshift/handlers"
+	"github.com/knutia/go-on-k8i_openshift/version"
+)
+
+func main() {
+	log.Printf(
+		"Starting the service...\ncommit: %s, build time: %s, release: %s",
+		version.Commit, version.BuildTime, version.Release,
+	)
+	log.Print("Starting the service...")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port is not set.")
+	}
+
+	router := handlers.Router(version.BuildTime, version.Commit, version.Release)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
+	}
+	go func() {
+		log.Fatal(srv.ListenAndServe())
+	}()
+	log.Print("The service is ready to listen and serve.")
+
+	killSignal := <-interrupt
+	switch killSignal {
+	case os.Interrupt:
+		log.Print("Got SIGINT...")
+	case syscall.SIGTERM:
+		log.Print("Got SIGTERM...")
+	}
+
+	log.Print("The service is shutting down...")
+	srv.Shutdown(context.Background())
+	log.Print("Done")
+}
